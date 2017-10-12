9b4b2437a222233d25b71650e385ac0dc42dcb43
halloween-jack <halloween.jack.dev@gmail.com>
2017-10-12 12:47:59 +0900

    Designing UICollectionView cells

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by slim on 2017/10/12.
+//  Copyright © 2017 halloween-jack. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    // MARK: Properties
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
